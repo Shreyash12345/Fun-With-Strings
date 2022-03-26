bcce62dd1ff7940ab6c4b0ff0611f8c39165ae96
Shreyash12345 <102417739+Shreyash12345@users.noreply.github.com>
2022-03-26 12:30:49 +0000

    Here Is My First File

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    String s1="I am mental";
+    s1=s1.replaceAll("e","a");
+    s1=s1.replace("I","You");
+    s1=s1.replaceFirst("a","ra");
+    System.out.print(s1);
+    
+    
+  }
+}
